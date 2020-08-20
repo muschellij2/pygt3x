ef89896f81bf8b3c3b663320df0759f1440db0bf
muschellij2 <muschellij2@gmail.com>
2020-08-20 13:09:44 -0400

    added a test

--- tests/testthat/test-check_reading.R
+++ tests/testthat/test-check_reading.R
@@ -2,12 +2,13 @@ path = system.file("extdata", "TAS1H30182785_2019-09-17.gt3x",
                    package = "pygt3x")
 
 testthat::test_that("read.gt3x and py_read_gt3x agree", {
-  rg = read.gt3x::read.gt3x(path, verbose = TRUE, debug = TRUE, asDataFrame = TRUE)
+  rg = read.gt3x::read.gt3x(path, verbose = FALSE,
+                            debug = FALSE, asDataFrame = TRUE)
   rg = rg[, c("X", "Y", "Z")]
   class(rg) = "data.frame"
 
 
-  res = py_read_gt3x(path, verbose = TRUE)
+  res = py_read_gt3x(path, verbose = FALSE)
   test = res$data
   zero = rowSums(test[, c("X", "Y", "Z")] == 0) == 3
   test = test[!zero, ]
